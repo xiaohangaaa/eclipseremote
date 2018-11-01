20c9ce59e3b59de45cf9be941841674c00f8369d
xiaohangaaa <44637416+xiaohangaaa@users.noreply.github.com>
2018-11-01 11:31:41 +0800

    Update Test01.java
    sadas

--- java01/src/com/itmayiedu/test/Test01.java
+++ java01/src/com/itmayiedu/test/Test01.java
@@ -4,7 +4,7 @@ public class Test01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("123456");
+		System.out.println("11111");
 		System.out.println("23456789");
 		System.out.println("23456789");
 
